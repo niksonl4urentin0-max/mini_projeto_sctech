7b32e984793626078a1cdbebcc89f049b0945d45
niksonl4urentin0-max <niksonlaurentino0@gmail.com>
2026-08-13 20:35:13 -0300

    salvando

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import csv
 import re
 import datetime
 
+physical_dims = ['product_weight_g', 'product_length_cm', 'product_height_cm', 'product_width_cm']
+dim1, dim2, dim3, dim4 = [],[],[],[]
 
+def product_validations(csv_row):
+    # VALIDACAO E TRATAMENTO DE DADOS AUSENTES - COL_PRODUCT_CATEGORY_NAME
+    if csv_row['product_category_name'].strip() == '':
+        csv_row['product_category_name'] = 'Sem Categoria' #CASO O NOME DA CATEGORIA ESTEJA VAZIO, ATRIBUIR O VALOR 'Sem Categoria'
+    else:
+        csv_row['product_category_name'] = csv_row['product_category_name'].strip()
 
-def read_data(csv_row):
-    for key, value in csv_row.items():
-        if csv_row['product_category_name'].strip() == '':
-            csv_row['product_category_name'] = 'Sem Categoria'
-        else:
-            csv_row['product_category_name'] = value.strip()   
+    
     return csv_row
 
+  
+#ABRINDO O ARQUIVO COM FUNCAO NATIVA PYTHON WITH OPEN
 with open ('olist_products_dataset.csv', 'r', encoding='utf-8') as f:
-    data = csv.DictReader(f)
-    for row in data:
-        print(read_data(row))
+    reader = csv.DictReader(f)
+    data = list(reader)    
+    for row in data:        
+        print(product_validations(row)) #CHAMANDO FUNCAO PARA TRATAR VALORES NULOS DE COLUNA product_category_name
+
+
