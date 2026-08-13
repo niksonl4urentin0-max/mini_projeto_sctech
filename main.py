47b18c2b1af1f9b4e54e452441f39f20b737e3f3
niksonl4urentin0-max <niksonlaurentino0@gmail.com>
2026-08-13 17:50:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import datetime
 
 def read_data(csv_row):
     for key, value in csv_row.items():
-        if not value or value.strip() == '':
-            csv_row[key] = 'Sem Categoria'
+        if csv_row['product_category_name'].strip() == '':
+            csv_row['product_category_name'] = 'Sem Categoria'
         else:
-            csv_row[key] = value.strip()   
+            csv_row['product_category_name'] = value.strip()   
     return csv_row
 
 with open ('olist_products_dataset.csv', 'r', encoding='utf-8') as f:
